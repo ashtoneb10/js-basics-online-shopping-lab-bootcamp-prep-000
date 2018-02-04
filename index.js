21f90db09090d893b356ad0401606f3635b6cc60
Ashton Blair <ashtoneb10@aol.com>
2018-02-04 18:48:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ function viewCart() {
     let trueValue = value[0]
     let key = Object.keys(object)
     let trueKey = key[0]
-    valueArray.push(trueValue)
-    keyArray.push(trueKey)
+    let valueArray = valueArray.push(trueValue)
+    let keyArray = keyArray.push(trueKey)
   } console.log(`In your cart, you have ${keyArray[0]} at $${valueArray[0]} and ${keyArray[1]} at $${valueArray[1]}.`)
 }
 }
