ce06e4b23bc33e8b18ae56d510c4ed293026d9ec
Ashton Blair <ashtoneb10@aol.com>
2018-02-04 17:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ function viewCart() {
  if(cart.length === 0){
    console.log('Your shopping cart is empty.') 
  } else if (cart.length === 1){
-   var Object = cart[0] 
    console.log(`In your cart, you have`)
  }
 }
