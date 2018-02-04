ca460a7441f9c7be47e6c44b962d8d4707b27686
Ashton Blair <ashtoneb10@aol.com>
2018-02-04 18:02:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   let numberPresent = cardNumber
-  if(!numberPresent){
+  if(numberPresent){
     console.log(`Sorry, we don't have a credit card on file for you.`)
   } else {
     console.log(`Your total cost is  , which will be charged to the card }.`)
