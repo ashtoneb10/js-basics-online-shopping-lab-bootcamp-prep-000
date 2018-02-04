8aab0b66a2d441d23d4bace41cc502dfc99e441e
Ashton Blair <ashtoneb10@aol.com>
2018-02-04 17:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
- if(cart.length === 0){
-   console.log("Your shopping cart is empty.")
- } else if(cart.length === 1) {
-   console.log(`In your cart, you have ${cart.item}.`)
- }
+ 
 }
 
 function total() {
