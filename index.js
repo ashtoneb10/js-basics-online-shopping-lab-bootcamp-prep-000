5eb13697c72ffecc72bef0c7ee71cddbc4fc1dfd
Ashton Blair <ashtoneb10@aol.com>
2018-02-04 18:59:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function viewCart() {
   let price3 = valueArray[2]
   console.log(`In your cart, you have ${item1} at $${price1}, ${item2} at $${price2}, and ${item3} at ${price3}.`)
 }
+}
 
 function total() {
   let total = 0;
