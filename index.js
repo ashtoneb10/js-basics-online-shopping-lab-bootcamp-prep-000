0b2df0e03d47c2aabae1c6cfae02475d4f74fc57
Ashton Blair <ashtoneb10@aol.com>
2018-02-04 18:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function removeFromCart(item) {
   for(let i = 0; i<cart.length; i++){
     let object = cart[i]
     if(object.hasOwnProperty(item)){
-      
+      cart.splice(i, 1)
     } else {
       total = total ++
     } 
