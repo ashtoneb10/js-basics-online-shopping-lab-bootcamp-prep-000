7631d97374c3f6d6e403eaffd21cb3ad1e78b940
Ashton Blair <ashtoneb10@aol.com>
2018-02-04 16:54:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,25 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var newItem = {
+   [item]: Math.floor(Math.random() * 100) + 1
+ }
+ cart.push(newItem);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+ if(cart.length === 0){
+   console.log("Your shopping cart is empty.")
+ } else if(cart.length === 1) {
+   console.log(`In your cart, you have ${cart.item}.`)
+ }
 }
 
 function total() {
-  // write your code here
+ var total = ;
+
 }
 
 function removeFromCart(item) {
@@ -26,5 +36,4 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
 }
