554feda659a0b252e7d3bc789d4d74c17fea2922
Ashton Blair <ashtoneb10@aol.com>
2018-02-04 17:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,4 +41,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  if(cardNumber){
+    let total = total()
+    console.log(`Your total cost is ${total} , which will be chared to the card ${cardNumber}.`)
+    cart = []
+  } else {
+    console.log(`Sorry, we don't have a credit card on file for you.`)
+  }
 }
